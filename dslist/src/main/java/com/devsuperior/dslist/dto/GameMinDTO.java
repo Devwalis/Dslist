857536214b33c0efe01a2551078075a6b3fb8f81
Walisson oliveira <walissonoliveira1@outlook.com>
2025-05-20 18:43:46 -0300

    Game repository, service, controller

---
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.model.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Integer getYear() {
+        return this.year;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public String getShortDescription() {
+        return this.shortDescription;
+    }
+   
+
+    public GameMinDTO(){
+
+         }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+   
+    
+
+}
